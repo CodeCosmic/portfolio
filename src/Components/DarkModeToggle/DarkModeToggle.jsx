d489818353b9d66cf04a310e01533f81cd77c55a
Roger <mroger612@gmail.com>
2023-04-10 13:54:14 -0500

    Switched default to be dark mode and renamed classNames

--- src/Components/DarkModeToggle/DarkModeToggle.jsx
+++ src/Components/DarkModeToggle/DarkModeToggle.jsx
@@ -7,14 +7,14 @@ const DarkToggle = () => {
 
     const checkHandler = (checked) => {
         setChecked(checked)
-        document.body.classList.toggle('dark-mode')
+        document.body.classList.toggle('light-mode')
         const modeIndicator = document.getElementById('modeIndicator')
         if(!checked){
-            modeIndicator.classList.remove('darkSpan')
-            modeIndicator.classList.add('lightSpan')
-        } else {
             modeIndicator.classList.remove('lightSpan')
             modeIndicator.classList.add('darkSpan')
+        } else {
+            modeIndicator.classList.remove('darkSpan')
+            modeIndicator.classList.add('lightSpan')
         }
        // const headerSpan = document.getElementById('headerSpan')
        // headerSpan.innerText = checked ? 'Dark mode is on' : 'Dark mode is off'
@@ -22,14 +22,14 @@ const DarkToggle = () => {
 
     return(
         <div className='darkModeSwitch'>
-            <span className='lightSpan' id='modeIndicator'>{checked ? 'Dark' : 'Light'}</span>
+            <span className='darkSpan' id='modeIndicator'>{checked ? 'Light' : 'Dark'}</span>
             <Switch
                 onChange={checkHandler}
                 checked={checked}
                 uncheckedIcon={false}
                 checkedIcon={false}
-                onColor='#24292e'
-                offColor='#ec1c24'
+                onColor='#ec1c24'
+                offColor='#24292e'
             />
         </div>
     )
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -18,20 +18,20 @@ const HomePage = () => {
     return(
     <div>
         <div className='container'>
-        {/*    <button className='homeLabelR' onClick={() => handleClick('about')} type="">{isVisible.about ? ''  : <p className='blink'>&gt;</p>}About</button>*/}
-        <span className="homeLabelR" onClick={() => handleClick('about')} >{isVisible.about ? ''  : <span className='blink'>&gt;</span>}About</span>
-            <p className={`aboutPR ${isVisible.about ? 'box' : ''}`} id='aboutParagraph' >
+        {/*    <button className='infoLabel' onClick={() => handleClick('about')} type="">{isVisible.about ? ''  : <p className='blink'>&gt;</p>}About</button>*/}
+        <span className="infoLabel" onClick={() => handleClick('about')} >{isVisible.about ? ''  : <span className='blink'>&gt;</span>}About</span>
+            <p className={`infoContent ${isVisible.about ? 'hide' : ''}`} id='aboutParagraph' >
                 Hello! My name is Roger. I hold a bachelors degree in Finance and worked as a
                 Data Analyst. While working as a Data Analyst I found my passion in coding and
-                decided to dive into coding. I completed a full stack bootcam from NuCamp in March
+                decided to dive into it so I completed a full stack bootcam from NuCamp in March
                 of 2023. I have about 8 months of experience in <span className='highlight'>full stack</span> development
                 with a focus on JavaScript, HTML and CSS and MERN stack technologies. I'm passionate
                 about creating webpages and I strive to make a positive impact in the world.
             </p>
         </div>
         <div className='container'>
-            <span className="homeLabelR" onClick={() => handleClick('education')}>{isVisible.education ? ''  : <span className='blink'>&gt;</span>}Skills</span>
-            <p className={`aboutPR ${isVisible.education ? 'box' : ''}`} id='educationParagraph'>
+            <span className="infoLabel" onClick={() => handleClick('education')}>{isVisible.education ? ''  : <span className='blink'>&gt;</span>}Skills</span>
+            <div className={`infoContent ${isVisible.education ? 'hide' : ''}`} id='educationParagraph'>
                 <ul>
                     <li>JavaScript</li>
                     <li>HTML</li>
@@ -41,11 +41,11 @@ const HomePage = () => {
                     <li>Express</li>
                     <li>Next.js</li>
                 </ul>
-            </p>
+            </div>
         </div>
         <div className='container'>
-            <span className='homeLabelR' onClick={() => handleClick('another')}>{isVisible.another ? ''  : <span className='blink'>&gt;</span>}Projects</span>
-            <p className={`aboutPR ${isVisible.another ? 'box' : ''}`} id='anotherParagraph'>
+            <span className='infoLabel' onClick={() => handleClick('another')}>{isVisible.another ? ''  : <span className='blink'>&gt;</span>}Projects</span>
+            <p className={`infoContent ${isVisible.another ? 'hide' : ''}`} id='anotherParagraph'>
                 Lorem ipsum dolor sit amet,
             </p>
         </div>
